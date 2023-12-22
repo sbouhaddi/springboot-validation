dd49534aea745e2868fdb0bd5e173ffb1f55ffe1
sbouhaddi <sabri.bouhaddi@gmail.com>
2023-12-22 20:09:01 +0100

    fix code smell

--- src/test/java/dev/sabri/validation/ValidationApplicationTests.java
+++ src/test/java/dev/sabri/validation/ValidationApplicationTests.java
@@ -1,13 +1,19 @@
 package dev.sabri.validation;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class ValidationApplicationTests {
 
+    @Autowired
+    private PersonController personController;
+
     @Test
     void contextLoads() {
+        Assertions.assertNotNull(personController);
     }
 
 }
